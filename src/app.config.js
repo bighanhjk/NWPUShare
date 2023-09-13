1624515f05e3aff6467b877160e9fbc374cc5a4d
bighanhjk <bighanhjk@qq.com>
2023-09-13 20:11:37 +0800

    移动或者删除原项目文件到新项目

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,10 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    "pages/home/index",
+    "pages/search/search",
+    "pages/message/message",
+    "pages/profile/profile"
   ],
   window: {
     backgroundTextStyle: 'light',
