815547b50c3b72d9d9a509db96cd5adfcfc9e8ae
bighanhjk <bighanhjk@qq.com>
2023-09-20 20:51:22 +0800

    修改mine页面样式

--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -1,53 +1,53 @@
 /* eslint-disable jsx-quotes */
 import Taro from "@tarojs/taro";
-import { View, Text, Image } from "@tarojs/components";
-import profile_top from "../../assets/images/profile_top.png";
+import { View, Text } from "@tarojs/components";
+// import profile_top from "../../assets/images/profile_top.png";
 import "./index.less";
 
 export default function Index() {
-  const onHandleLogin = () => {
-    console.log("login");
-  };
+  // const onHandleLogin = () => {
+  //   console.log("login");
+  // };
   return (
     <View className="index">
-      <View className="login-wrapper">
-        <View className="login_icon"></View>
-        <View
-          className="login_text"
-          onClick={() => Taro.navigateTo({ url: "/pages/npulogin/index" })}
-        >
-          <Text>登录</Text>
-        </View>
-        <Image className="topimage" src={profile_top} mode="aspectFill" />
-      </View>
-      <View className="function_wrapper">
-        <View className="function_border">
-          <View className="function_item">
-            <Text className="itemtext">我的订单</Text>
-          </View>
-
-          <View className="function_item">
-            <Text className="itemtext">共享商品</Text>
-          </View>
-
-          <View className="function_item">
-            <Text className="itemtext">二手商品</Text>
-          </View>
-
-          <View className="function_item">
-            <Text className="itemtext">意见反馈</Text>
-          </View>
-
-          <View className="function_item">
-            <Text className="itemtext">使用说明</Text>
-          </View>
-
-          <View className="function_item">
-            <Text className="itemtext">我的足迹</Text>
+      <View className="head-wrapper">
+        {/* <Image
+          className="head-wrapper-image"
+          src={profile_top}
+          mode="widthFix"
+        /> */}
+        <View className="head-wrapper-login-wrapper">
+          <View className="head-wrapper-login-wrapper-icon"></View>
+          <View
+            className="head-wrapper-login-wrapper-text"
+            onClick={() => Taro.navigateTo({ url: "/pages/npulogin/index" })}
+          >
+            <Text>登录</Text>
           </View>
         </View>
       </View>
-
+      <View className="body-wrapper">
+        <View className="body-wrapper-border">
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">我的订单</Text>
+          </View>
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">共享商品</Text>
+          </View>
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">二手商品</Text>
+          </View>
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">意见反馈</Text>
+          </View>
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">使用说明</Text>
+          </View>
+          <View className="body-wrapper-border-item">
+            <Text className="body-wrapper-border-item-text">我的足迹</Text>
+          </View>
+        </View>
+      </View>
       <View className="version">版本0.0.1</View>
     </View>
   );
